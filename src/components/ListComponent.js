1eaeecb68866e068f231c2be49da036956371e82
Shylmysten <TBoswell@kc.rr.com>
2018-08-20 19:56:40 -0500

    feat: new ListItem component

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PBFS from '../imgs/Powered-by-Foursquare-black-300.png';
+import ListItem from './ListItem'
 
 class ListComponent extends React.Component {
   render() {
@@ -12,17 +13,7 @@ class ListComponent extends React.Component {
           <div className="list-top-spacer"></div>
             <ul>
 
-              <li className="restaurant-list-item">
-                <img aria-hidden="true" className="list-image" src="https://lh4.googleusercontent.com/-9f4Vp0wMENA/WXCuIIFyABI/AAAAAAAAAME/QtKJtspF7qcQVGxiZpHRrsVRx73qLpENwCLIBGAYYCw/w240-h240-p-k-no/" alt="A Great restaurant"/>
-                <div className="list-info-wrapper">
-                  <h5 className="list-title">Smokehouse Barbecue - Kansas City, Missouri</h5>
-                  <p className="rating">4.0 - AWESOME!!!<span className="review-count">  342 reviews</span></p>
-                  <p className="cuisines">Barbecue Restaurant</p>
-                  <p className="address">8451 NW Prairie View Rd, Kansas City, MO 64152</p>
-                  <p className="hours-status">Open until 9:00 PM</p>
-
-                </div>
-              </li>
+              <ListItem/>
 
             </ul>
           <div className="list-bottom-spacer"><img src={PBFS} alt="Powered by Foursquare"/></div>
---
+++ src/components/ListItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class ListItem extends React.Component {
+  render() {
+    return (
+      <li className="restaurant-list-item">
+        <img aria-hidden="true" className="list-image" src="https://lh4.googleusercontent.com/-9f4Vp0wMENA/WXCuIIFyABI/AAAAAAAAAME/QtKJtspF7qcQVGxiZpHRrsVRx73qLpENwCLIBGAYYCw/w240-h240-p-k-no/" alt="A Great restaurant"/>
+        <div className="list-info-wrapper">
+          <h5 className="list-title">Smokehouse Barbecue - Kansas City, Missouri</h5>
+          <p className="rating">4.0 - AWESOME!!!<span className="review-count">  342 reviews</span></p>
+          <p className="cuisines">Barbecue Restaurant</p>
+          <p className="address">8451 NW Prairie View Rd, Kansas City, MO 64152</p>
+          <p className="hours-status">Open until 9:00 PM</p>
+
+        </div>
+      </li>
+    )
+  }
+}
+
+export default ListItem
