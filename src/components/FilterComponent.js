f02e0f61c2e25921f11e0134d5af20c0f3e14c1b
Shylmysten <TBoswell@kc.rr.com>
2018-08-22 01:09:48 -0500

    feat: add clearQuery refactor: change placeholder text

--- src/components/FilterComponent.js
+++ src/components/FilterComponent.js
@@ -12,12 +12,13 @@ class FilterComponent extends React.Component {
         <div className="filterContainer">
           <div className="filterIcon"></div>
           <input
+            value={this.props.query}
             className="filterText"
             type="text"
-            placeholder="barbecue restaurants"
+            placeholder="enter text to filter results"
             onChange={this.handleChange.bind(this)}
           />
-          <button className="close"></button>
+          <button className="clearQuery" onClick={()=> this.props.clearQuery()}></button>
         </div>
       </section>
     );
