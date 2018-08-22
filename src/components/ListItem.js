516348fe4d4d3f3f6706c32de47073acb2fa8c1e
Shylmysten <TBoswell@kc.rr.com>
2018-08-21 22:28:16 -0500

    refactor: refactor for easier readability

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,21 +4,32 @@ import noImage from '../imgs/restaurantImages/noImage.png'
 class ListItem extends React.Component {
   render() {
     const [...restaurants] = this.props.listData
-
     return (
       <ul>
-      {restaurants.map(restaurant => {
-        let place = restaurant.venue;
+      {restaurants.map(place => {
         return (
-          <li className="restaurant-list-item" key={place.id}>
-            <img aria-hidden="true" className="list-image" src={noImage} alt="A Great restaurant"/>
+          <li
+            className="restaurant-list-item"
+            key={place.id}>
+
+            <img
+              aria-hidden="true"
+              className="list-image"
+              src={!place.bestPhoto.prefix ? noImage : place.bestPhoto.prefix +'80x80'+ place.bestPhoto.suffix}
+              alt={!place.description ? place.name : place.description}
+            />
 
             <div className="list-info-wrapper">
+
               <h5 className="list-title">{place.name}</h5>
-              <p className="rating">4.0 - AWESOME!!!<span className="review-count">342 reviews</span></p>
+
+              <p className="rating">Rating: {place.rating} out of 10<span className="review-count">{place.likes.count} reviews</span></p>
+
               <p className="cuisines">Barbecue Restaurant</p>
+
               <p className="address">{place.location.address+', '+ place.location.formattedAddress[1]}</p>
-              <p className="hours-status">Open until 9:00 PM</p>
+
+              <p className="hours-status">{place.hours.status}</p>
 
             </div>
           </li>
