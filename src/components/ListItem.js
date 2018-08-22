08788d904783b13cb1cc2a7d45bd72b99b4cbe17
Shylmysten <TBoswell@kc.rr.com>
2018-08-21 20:12:15 -0500

    feat: add props to list items

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,20 +1,31 @@
 import React from 'react'
+import noImage from '../imgs/restaurantImages/noImage.png'
 
 class ListItem extends React.Component {
   render() {
+    const [...restaurants] = this.props.listData
+
     return (
-      <li className="restaurant-list-item">
-        <img aria-hidden="true" className="list-image" src="https://lh4.googleusercontent.com/-9f4Vp0wMENA/WXCuIIFyABI/AAAAAAAAAME/QtKJtspF7qcQVGxiZpHRrsVRx73qLpENwCLIBGAYYCw/w240-h240-p-k-no/" alt="A Great restaurant"/>
+      <ul>
+      {restaurants.map(restaurant => {
+        let place = restaurant.venue;
+        return (
+          <li className="restaurant-list-item" key={place.id}>
+            <img aria-hidden="true" className="list-image" src={noImage} alt="A Great restaurant"/>
 
-        <div className="list-info-wrapper">
-          <h5 className="list-title">Smokehouse Barbecue - Kansas City, Missouri</h5>
-          <p className="rating">4.0 - AWESOME!!!<span className="review-count">  342 reviews</span></p>
-          <p className="cuisines">Barbecue Restaurant</p>
-          <p className="address">8451 NW Prairie View Rd, Kansas City, MO 64152</p>
-          <p className="hours-status">Open until 9:00 PM</p>
+            <div className="list-info-wrapper">
+              <h5 className="list-title">{place.name}</h5>
+              <p className="rating">4.0 - AWESOME!!!<span className="review-count">342 reviews</span></p>
+              <p className="cuisines">Barbecue Restaurant</p>
+              <p className="address">{place.location.address+', '+ place.location.formattedAddress[1]}</p>
+              <p className="hours-status">Open until 9:00 PM</p>
 
-        </div>
-      </li>
+            </div>
+          </li>
+
+        )
+    })}
+    </ul>
     )
   }
 }
